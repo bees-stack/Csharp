264e08bbaba7847c51bebbde239c16c4d0223cd3
LEEYANGY <56088510+LEEYANGY@users.noreply.github.com>
2020-11-01 03:12:12 +0800

    first commit (第一次提交)
    尚未完善，学习能力有限，勿喷

---
+++ ConnectionDatabase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace WindowsFormsApp2
+{
+    class ConnectionDatabase
+    {
+        //定义sqlConnection，判断是否连接成功,直接初始化
+        public static  SqlConnection sqlConnection=null;
+        //新建一个连接字符串
+        public static string sqlConnectionStr = @"Data Source=LEEYANGY\LEEYANGY;Initial Catalog=Student;Integrated Security=True";
+
+        //i定义Sql_Connection()与数据库建立连接，Sql_Connection()后续还需要被调用
+        public static SqlConnection Sql_Connection() {
+            sqlConnection = new SqlConnection(sqlConnectionStr);
+            sqlConnection.Open();
+            return sqlConnection;
+        }
+
+        //关闭数据库连接
+        public void sqlConnection_close() {
+            if (sqlConnection.State == ConnectionState.Open) {
+                //关闭并释放资源
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+            }
+        }
+
+        //Command查询
+        public  SqlCommand command(string sqlStr) {
+            //连接数据库
+            Sql_Connection();
+            //实例化对象，把值传入
+            SqlCommand cmd = new SqlCommand(sqlStr,sqlConnection);
+            return cmd;
+        }
+
+        //
+        public int Excute(string sqlStr) {
+            return command(sqlStr).ExecuteNonQuery();
+        }
+
+
+        //读取数据库中的内容
+        public SqlDataReader read(string sqlStr)
+        {
+            //连接数据库
+            Sql_Connection();
+            return command(sqlStr).ExecuteReader();
+        }
+
+
+
+
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string UID = textBox1.Text.Trim();
+            string UPAW = textBox2.Text.Trim();
+            //非空验证
+            if (UID == "" && UPAW == "") {
+                MessageBox.Show("重新输入哦");
+            }
+            //实例化对象
+            ConnectionDatabase cd = new ConnectionDatabase();
+            string sqlStr = "select * from login_in where username='"+textBox1.Text+"' and password='"+textBox2.Text+"'";
+            //读入数据库操作  执行完操作后dr已经读到上一条语句后的内容
+            IDataReader dr = cd.read(sqlStr);
+            if (dr.Read())
+            {
+                MessageBox.Show("登录成功,即将跳转到下一个页面");
+                Form2 f2 = new Form2();
+                f2.Show();
+                this.Hide();
+            }
+            else {
+                MessageBox.Show("登录失败");
+                return ;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3 f3 = new Form3();
+            f3.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            Application.ExitThread();
+        }
+    }
+}
